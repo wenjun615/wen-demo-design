9c9ee32ce67f413ff4c5caf099473a318339cc76
wenjun <835045686@qq.com>
2022-06-02 13:42:14 +0800

    抽象工厂模式

---
+++ design-2-02/src/main/java/design/factory/impl/EGMCacheAdapter.java
@@ -0,0 +1,31 @@
+package design.factory.impl;
+
+import design.factory.ICacheAdapter;
+import design.matter.EGM;
+
+import java.util.concurrent.TimeUnit;
+
+public class EGMCacheAdapter implements ICacheAdapter {
+
+    private final EGM egm = new EGM();
+
+    @Override
+    public String get(String key) {
+        return egm.gain(key);
+    }
+
+    @Override
+    public void set(String key, String value) {
+        egm.set(key, value);
+    }
+
+    @Override
+    public void set(String key, String value, long timeout, TimeUnit timeUnit) {
+        egm.setEx(key, value, timeout, timeUnit);
+    }
+
+    @Override
+    public void del(String key) {
+        egm.delete(key);
+    }
+}
---
+++ design-2-02/src/main/java/design/factory/impl/IIRCacheAdapter.java
@@ -0,0 +1,32 @@
+package design.factory.impl;
+
+import design.factory.ICacheAdapter;
+import design.matter.IIR;
+
+import java.util.concurrent.TimeUnit;
+
+public class IIRCacheAdapter implements ICacheAdapter {
+
+    private final IIR iir = new IIR();
+
+    @Override
+    public String get(String key) {
+        return iir.get(key);
+    }
+
+    @Override
+    public void set(String key, String value) {
+        iir.set(key, value);
+    }
+
+    @Override
+    public void set(String key, String value, long timeout, TimeUnit timeUnit) {
+        iir.setExpire(key, value, timeout, timeUnit);
+    }
+
+    @Override
+    public void del(String key) {
+        iir.del(key);
+    }
+
+}
